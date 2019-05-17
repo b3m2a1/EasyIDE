4c3e7d72ec6dfb95f675c0cfde9b1aa316ee09f0
b3m2a1 <b3m2a1@gmail.com>
2019-05-17 00:57:48 -0700

    Committed EasyIDE application @ 2019-05-17_00:57:48

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,8 +1,6 @@
-(* ::Package:: *)
-
 Paclet[
   Name -> "EasyIDE",
-  Version -> "1.0.13",
+  Version -> "1.0.14",
   Creator -> "b3m2a1 <b3m2a1@gmail.com>",
   URL -> "https://github.com/b3m2a1/EasyIDE",
   Description -> "An IDE for Mathematica written entirely within Mathematica",
